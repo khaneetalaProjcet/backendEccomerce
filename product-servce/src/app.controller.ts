a2d0ab66acd0d8761956192f991ce20b9d50a04c
javad <mahdipourjavad1379@gmail.com>
2025-06-10 15:31:12 +0330

    kafka service

--- product-servce/src/app.controller.ts
+++ product-servce/src/app.controller.ts
@@ -1,24 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { EventPattern, Payload, Ctx, KafkaContext } from '@nestjs/microservices';
+import {KafkaProducerService} from "../src/kafka/kafka.producer"
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @EventPattern('order_created')
-  async handleOrderCreated(@Payload() message: any, @Ctx() context: KafkaContext) {
-    const offset = context.getMessage().offset;
-    const partition = context.getPartition();
-    console.log(`Received message on partition ${partition}, offset ${offset}`);
-    console.log('Order data:', message.value);
-
-    return offset
-  }
-
+  constructor(private readonly appService: AppService,private readonly kafkaService:KafkaProducerService) {}
 
   @Get()
-  getHello(): string {
+  async getHello() {
+    await this.kafkaService.sendMessage('order_created', { orderId: '123', status: 'new',value:"salllll" });
     return this.appService.getHello();
   }
 }
