9a2934aab5331ebba765331b92d9b8ebc4e9f7f0
hossein khodakhah <elham.chatrabnoos@gmail.com>
2025-07-14 16:29:16 +0330

    asdf

--- wallet-service/src/app.service.ts
+++ wallet-service/src/app.service.ts
@@ -37,7 +37,7 @@ export class AppService {
       TerminalId: process.env.SEP_TERMINAL_ID,
       Amount: order.totalPrice,
       ResNum: await this.generator(),
-      RedirectUrl: 'https://ecommerce.khaneetala.ir/api/redierct',
+      RedirectUrl: 'https://ecommerce.khaneetala.ir/api/redirect',
       CellNumber: '09229055682',
     };
 
