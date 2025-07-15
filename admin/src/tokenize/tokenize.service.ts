7506e9f5a649299c6bf162da4741fd8bf5f1c42f
fafkhe <115066487+fafkhe@users.noreply.github.com>
2025-07-15 12:24:30 +0330

    refactor generating token for admin

--- admin/src/tokenize/tokenize.service.ts
+++ admin/src/tokenize/tokenize.service.ts
@@ -4,49 +4,48 @@ import { userToken } from 'src/interfaces/interfaces';
 
 @Injectable()
 export class TokenizeService {
+  constructor(private readonly jwt: JwtService) {}
 
-    constructor(private readonly jwt : JwtService){
-
+  async tokenize(data: userToken, time: string, type: number) {
+    /**
+     * this is for generating access token
+     */
+    if (type == 0) {
+      return this.jwt.sign(data, {
+        secret: process.env.JWT_USER_SECRET,
+        expiresIn: time,
+      });
     }
 
-
-    async tokenize(data : userToken ,  time : string , type : number){
-        /**
-         * this is for generating access token
-         */
-        if (type == 0){
-            return this.jwt.sign(data, {
-                secret: process.env.JWT_USER_SECRET,
-                expiresIn: time
-            })
-        }
-
-
-        /**
-         * this is for generating refreshToken
-         */
-        if (type == 1){
-            return this.jwt.sign(data, {
-                secret: process.env.JWT_REFRESH_SECRET,
-                expiresIn: time
-            })
-        }
+    /**
+     * this is for generating refreshToken
+     */
+    if (type == 1) {
+      return this.jwt.sign(data, {
+        secret: process.env.JWT_REFRESH_SECRET,
+        expiresIn: time,
+      });
     }
 
-
-    async checkRefreshToken(token: string) {
-        try {
-            let decoded = await this.jwt.verify(token, {
-                secret: process.env.JWT_REFRESH_SECRET
-            })
-            if (!decoded) {
-                return false
-            }
-            return decoded
-        } catch (error) {
-            return false
-        }
+    if (type == 2) {
+      return this.jwt.sign(data, {
+        secret: process.env.JWT_ADMIN_SECRET,
+        expiresIn: time,
+      });
     }
+  }
 
-
+  async checkRefreshToken(token: string) {
+    try {
+      let decoded = await this.jwt.verify(token, {
+        secret: process.env.JWT_REFRESH_SECRET,
+      });
+      if (!decoded) {
+        return false;
+      }
+      return decoded;
+    } catch (error) {
+      return false;
+    }
+  }
 }
