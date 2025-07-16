43ba2023653341faa495a56ea2b63a01f362e98e
hossein khodakhah <elham.chatrabnoos@gmail.com>
2025-07-16 14:44:09 +0330

    asdf

--- walletSideCar/src/crob-job/crob-job.service.ts
+++ walletSideCar/src/crob-job/crob-job.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { Interval } from '@nestjs/schedule';
+import { Model } from 'mongoose';
+import { goldInvoice, goldInvoiceInterface } from 'src/wallet/entities/goldBoxInvoice.entity';
 
 
 
 @Injectable()
 export class CrobJobService {
 
+    constructor(@InjectModel(goldInvoice.name) private goldInvoiceModel : Model<goldInvoiceInterface>){}
+
+
     @Interval('goldBox', 10000)
-    handleCronEveryMinute() {
-        console.log('test pass')
+    async handleCronEveryMinute() {
+
+        let pendings = await this.goldInvoiceModel.find({
+            status : 'pending',
+            state : 1,
+        })
+
+        console.log('pendings list is>>>>>' , pendings)
     }
 
 
